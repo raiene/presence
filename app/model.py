3be17a9c0b581dd73e60125265c2036d95ab1fe1
Raiene <raienesousa@mpto.mp.br>
2021-02-24 19:11:32 -0300

    Corrige model

--- app/model.py
+++ app/model.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from app import db #, admin
 from flask_mongoengine.wtf import model_form
 from wtforms import Form, StringField, SelectField
-from flask_admin.contrib.mongoengine import ModelView
+# from flask_admin.contrib.mongoengine import ModelView
 # from flask_admin.actions import action
 
 class Culto(db.Document):
