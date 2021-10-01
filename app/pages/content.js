374081734a3416650906994defb72274d7dec3b2
Raiene <raienepontes@gmail.com>
2021-10-01 14:10:36 -0300

    Ajusta erros pro build (#2)
    Co-authored-by: Raiene Pontes <raienesousa@mpto.mp.br>

--- app/pages/content.js
+++ app/pages/content.js
@@ -2,9 +2,9 @@ import Base from './preform'
 
 export default function Content() {
     return (
-        <div class="limiter">
-        <div class="container-login100"> 
-            <div class="wrap-login100">
+        <div className="limiter">
+        <div className="container-login100"> 
+            <div className="wrap-login100">
                 <Base></Base>
             </div>
         </div>
