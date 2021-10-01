9d2ea3d663c8e2bafffd83a5fb40dd617da14234
Raiene Pontes <raienesousa@mpto.mp.br>
2021-10-01 12:08:49 -0300

    Ajusta erros pro build

--- app/pages/form.js
+++ app/pages/form.js
@@ -1,30 +1,30 @@
 export default function Form(){
     return (
         <div>
-            <span class="login100-form-title p-b-34 p-t-27">
+            <span className="login100-form-title p-b-34 p-t-27">
                 Confirme sua presença no culto
                 {/* {#{ "{}/{}/{}".format(culto.dt_culto.day, culto.dt_culto.month, culto.dt_culto.year) }#} */}
             </span>
-            {/* <!-- <p class="error">{{ form.errors }}</p> --> */}
-            <div class="wrap-input100 validate-input" data-validate="Enter name">
+            {/* <!-- <p className="error">{{ form.errors }}</p> --> */}
+            <div className="wrap-input100 validate-input" data-validate="Enter name">
                 {/* <!-- <label for="nome">Inscrição individual</label> --> */}
-                <input class="input100" type="text" name="nome" placeholder="seu nome" maxlength="20" />
-                <span class="focus-input100" data-placeholder="&#xf207;"></span>
+                <input className="input100" type="text" name="nome" placeholder="seu nome" />
+                <span className="focus-input100" data-placeholder="&#xf207;"></span>
             </div>
-            <div class="contact100-form">
-                <select class="form-control" id="culto" name="culto">
+            <div className="contact100-form">
+                <select className="form-control" id="culto" name="culto">
                     {/* {% for c in form.culto %}
                         {{ c }}
                     {% endfor %} */}
                 </select>
             </div>
             <br />
-            <div class="container-login100-form-btn">
-                <button name="action_save" class="login100-form-btn" type="submit">
+            <div className="container-login100-form-btn">
+                <button name="action_save" className="login100-form-btn" type="submit">
                     Confirme
                 </button>
             </div>
-            {/* <span class="login100-form-title p-b-34 p-t-27">
+            {/* <span className="login100-form-title p-b-34 p-t-27">
                 Nenhum culto disponível
             </span> */}
         </div>
--- app/pages/preform.js
+++ app/pages/preform.js
@@ -1,9 +1,10 @@
+
 import Form from './form'
 
 export default function Preform(){
     return (
-        <form class="login100-form validate-form" method="post" role="form" >
-        <span class="login100-form-logo">
+        <form className="login100-form validate-form" method="post" role="form" >
+        <span className="login100-form-logo">
             <img src="/iconeredondo.png" alt="Logo IPB" width="125" height="125" />
         </span>
         <Form></Form>
